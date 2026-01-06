4e59261030847aede95756c203f48b0c64e52fe9
Lantz Brown <lantzbrown@Lantzs-MacBook-Air.local>
2026-01-06 16:47:34 +0100

    Update gallery with new ImageKit photos and keep original photos

--- src/screens/Desktop/DesktopGallery.tsx
+++ src/screens/Desktop/DesktopGallery.tsx
@@ -4,106 +4,46 @@ import { Separator } from "../../components/ui/separator";
 
 // Desktop gallery images
 const desktopGalleryImages = [
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-2-1.png",
-    width: "661px",
-    height: "496px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-1-1.png",
-    width: "662px",
-    height: "882px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-20-1.png",
-    width: "661px",
-    height: "881px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-3-1.png",
-    width: "661px",
-    height: "496px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-7-1.png",
-    width: "661px",
-    height: "930px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-18-1.png",
-    width: "661px",
-    height: "496px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-19-1.png",
-    width: "662px",
-    height: "882px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-17-1.png",
-    width: "661px",
-    height: "881px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-6-1.png",
-    width: "661px",
-    height: "881px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-11-1.png",
-    width: "661px",
-    height: "881px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping--1.png",
-    width: "661px",
-    height: "881px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-16-1.png",
-    width: "661px",
-    height: "881px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-12-1.png",
-    width: "661px",
-    height: "881px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-4-1.png",
-    width: "661px",
-    height: "881px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-14-1.png",
-    width: "661px",
-    height: "881px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-13-1.png",
-    width: "661px",
-    height: "881px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-15-1.png",
-    width: "661px",
-    height: "661px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-8-1.png",
-    width: "661px",
-    height: "881px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-5-1.png",
-    width: "661px",
-    height: "881px",
-  },
-  {
-    src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-10-1.png",
-    width: "661px",
-    height: "881px",
-  },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.01.56.jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.00.jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.01.59.jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.00%20(2).jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.01.56%20(2).jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.01.56%20(3).jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.00%20(7).jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.01.jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.00%20(1).jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.00%20(3).jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.59.jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.58%20(2).jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.00%20(4).jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.58%20(1).jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.58.jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.00%20(6).jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.00%20(5).jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.57.jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.58%20(4).jpeg" },
+  { src: "https://ik.imagekit.io/lantz/Sabas/WhatsApp%20Image%202026-01-05%20at%2020.02.58%20(3).jpeg" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-2-1.png", width: "661px", height: "496px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-1-1.png", width: "662px", height: "882px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-20-1.png", width: "661px", height: "881px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-3-1.png", width: "661px", height: "496px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-7-1.png", width: "661px", height: "930px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-18-1.png", width: "661px", height: "496px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-19-1.png", width: "662px", height: "882px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-17-1.png", width: "661px", height: "881px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-6-1.png", width: "661px", height: "881px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-11-1.png", width: "661px", height: "881px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping--1.png", width: "661px", height: "881px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-16-1.png", width: "661px", height: "881px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-12-1.png", width: "661px", height: "881px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-4-1.png", width: "661px", height: "881px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-14-1.png", width: "661px", height: "881px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-13-1.png", width: "661px", height: "881px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-15-1.png", width: "661px", height: "661px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-8-1.png", width: "661px", height: "881px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-5-1.png", width: "661px", height: "881px" },
+  { src: "https://c.animaapp.com/mdhv6xukEmiG5d/img/xi-landscaping-10-1.png", width: "661px", height: "881px" },
 ];
 
 // Footer links
@@ -125,32 +65,32 @@ export const DesktopGallery = (): JSX.Element => {
         </Link>
 
         <nav className="absolute top-[50px] right-10 flex space-x-[65px]">
-          <Link 
-            to="/" 
+          <Link
+            to="/"
             className="font-['Inter',Helvetica] font-normal text-white text-[22px] tracking-[0] leading-[normal] hover:underline"
           >
             HOME
           </Link>
-          <Link 
-            to="/services" 
+          <Link
+            to="/services"
             className="font-['Inter',Helvetica] font-normal text-white text-[22px] tracking-[0] leading-[normal] hover:underline"
           >
             SERVICES
           </Link>
-          <Link 
-            to="/gallery" 
+          <Link
+            to="/gallery"
             className="font-['Inter',Helvetica] font-normal text-white text-[22px] tracking-[0] leading-[normal] hover:underline"
           >
             GALLERY
           </Link>
-          <Link 
-            to="/contact" 
+          <Link
+            to="/contact"
             className="font-['Inter',Helvetica] font-normal text-white text-[22px] tracking-[0] leading-[normal] hover:underline"
           >
             CONTACT
           </Link>
         </nav>
-        
+
         {/* Page Title */}
         <div className="absolute bottom-[60px] left-10">
           <h1 className="font-semibold text-white text-8xl tracking-[0] leading-[normal]">
@@ -182,57 +122,57 @@ export const DesktopGallery = (): JSX.Element => {
       {/* Footer */}
       <footer className="relative w-full bg-ligth-gray py-12">
         <div className="container mx-auto px-4">
-        <h3 className="font-['Inter',Helvetica] font-light text-[#202e13] text-[28px] lg:text-[38px] tracking-[0] leading-[normal] mb-8">
-          XI Landscaping
-        </h3>
-        <div className="flex flex-col md:flex-row items-start md:items-center space-y-4 md:space-y-0 md:space-x-[40px] mb-8">
-          <span className="font-['Inter',Helvetica] font-semibold text-[#202e13] text-[18px] lg:text-[22px] tracking-[0] leading-[normal]">
-            SITEMAP
-          </span>
+          <h3 className="font-['Inter',Helvetica] font-light text-[#202e13] text-[28px] lg:text-[38px] tracking-[0] leading-[normal] mb-8">
+            XI Landscaping
+          </h3>
+          <div className="flex flex-col md:flex-row items-start md:items-center space-y-4 md:space-y-0 md:space-x-[40px] mb-8">
+            <span className="font-['Inter',Helvetica] font-semibold text-[#202e13] text-[18px] lg:text-[22px] tracking-[0] leading-[normal]">
+              SITEMAP
+            </span>
 
-          <div className="flex flex-wrap gap-4 md:gap-[40px]">
-            <Link
-              to="/"
-              onClick={() => window.scrollTo(0, 0)}
-              className="font-['Inter',Helvetica] font-normal text-[#6f6f6f] text-[18px] lg:text-[22px] tracking-[0] leading-[normal]"
-            >
-              HOME
-            </Link>
-            <Link
-              to="/services"
-              onClick={() => window.scrollTo(0, 0)}
-              className="font-['Inter',Helvetica] font-normal text-[#6f6f6f] text-[18px] lg:text-[22px] tracking-[0] leading-[normal]"
-            >
-              SERVICES
-            </Link>
-            <Link
-              to="/gallery"
-              onClick={() => window.scrollTo(0, 0)}
-              className="font-['Inter',Helvetica] font-normal text-[#6f6f6f] text-[18px] lg:text-[22px] tracking-[0] leading-[normal]"
-            >
-              GALLERY
-            </Link>
-            <Link
-              to="/contact"
-              onClick={() => window.scrollTo(0, 0)}
-              className="font-['Inter',Helvetica] font-normal text-[#6f6f6f] text-[18px] lg:text-[22px] tracking-[0] leading-[normal]"
-            >
-              CONTACT
-            </Link>
-          </div>
-        </div>
-
-        <Separator className="w-full mb-6" />
-
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="font-['Inter',Helvetica] font-normal text-[#6f6f6f] text-[18px] lg:text-[22px] tracking-[0] leading-[normal] mb-4 md:mb-0">
-            COURTESY 2025. ALL RIGHTS RESERVED.
+            <div className="flex flex-wrap gap-4 md:gap-[40px]">
+              <Link
+                to="/"
+                onClick={() => window.scrollTo(0, 0)}
+                className="font-['Inter',Helvetica] font-normal text-[#6f6f6f] text-[18px] lg:text-[22px] tracking-[0] leading-[normal]"
+              >
+                HOME
+              </Link>
+              <Link
+                to="/services"
+                onClick={() => window.scrollTo(0, 0)}
+                className="font-['Inter',Helvetica] font-normal text-[#6f6f6f] text-[18px] lg:text-[22px] tracking-[0] leading-[normal]"
+              >
+                SERVICES
+              </Link>
+              <Link
+                to="/gallery"
+                onClick={() => window.scrollTo(0, 0)}
+                className="font-['Inter',Helvetica] font-normal text-[#6f6f6f] text-[18px] lg:text-[22px] tracking-[0] leading-[normal]"
+              >
+                GALLERY
+              </Link>
+              <Link
+                to="/contact"
+                onClick={() => window.scrollTo(0, 0)}
+                className="font-['Inter',Helvetica] font-normal text-[#6f6f6f] text-[18px] lg:text-[22px] tracking-[0] leading-[normal]"
+              >
+                CONTACT
+              </Link>
+            </div>
           </div>
 
-          <div className="font-['Inter',Helvetica] font-normal text-[#6f6f6f] text-[18px] lg:text-[22px] tracking-[0] leading-[normal]">
-            PRIVACY POLICY | TERMS & CONDITIONS
+          <Separator className="w-full mb-6" />
+
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className="font-['Inter',Helvetica] font-normal text-[#6f6f6f] text-[18px] lg:text-[22px] tracking-[0] leading-[normal] mb-4 md:mb-0">
+              COURTESY 2025. ALL RIGHTS RESERVED.
+            </div>
+
+            <div className="font-['Inter',Helvetica] font-normal text-[#6f6f6f] text-[18px] lg:text-[22px] tracking-[0] leading-[normal]">
+              PRIVACY POLICY | TERMS & CONDITIONS
+            </div>
           </div>
-        </div>
         </div>
       </footer>
     </div>
